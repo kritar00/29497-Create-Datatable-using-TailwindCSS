1a788cf94d44c4d3c980c94531bf2dc38cbf02d2
Thien Phu <nttp.220401@gmail.com>
2022-11-04 08:30:01 +0700

    4/11/2022

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import getDate from "./helpers.js"
+import getDate from "./modules/helpers.js"
+import apiCall from "./modules/API.js"
 
 let list = document.getElementById("list")
 let titleInput = document.getElementById('titleInput')
@@ -13,38 +14,28 @@ async function displayData(props) {
     props.forEach(function (props, index) {
         let { id, title, content, createdAt, image } = props;
         productItem += ` 
-              <tr class="item-wrapper odd:bg-white even:bg-stone-200">
-              <td class="whitespace-nowrap text-blue-500 font-bold hover:underline">${id}</td>
-              <td class="whitespace-nowrap">${createdAt}</td>
-              <td class="whitespace-nowrap">${title}</td>
-              <td class="w-16 whitespace-nowrap"><img src="${image}"></td>
-              <td class="whitespace-nowrap">${content}
-              <button data-id=${id} class="float-right remove-item"><i class="uil uil-times pointer-events-none text-3xl"></i></button>
-              <button data-id=${id} class="float-right edit-item"><i class="uil uil-edit-alt pointer-events-none text-3xl"></i></button>
-              </td>
+            <tr class="item-wrapper odd:bg-white even:bg-stone-200">
+            <td class="whitespace-nowrap text-blue-500 font-bold hover:underline">${id}</td>
+            <td class="whitespace-nowrap">${createdAt}</td>
+            <td class="whitespace-nowrap">${title}</td>
+            <td class="w-16 whitespace-nowrap"><img src="${image}"></td>
+            <td class="whitespace-nowrap">${content}
+            <button data-id=${id} class="float-right remove-item"><i class="uil uil-times pointer-events-none text-3xl"></i></button>
+            <button data-id=${id} class="float-right edit-item"><i class="uil uil-edit-alt pointer-events-none text-3xl"></i></button>
+            </td>
             </tr> `
         list.innerHTML = productItem;
     })
 }
+
 getData()
 //GET method
-async function getData(id) {
-    const http = new XMLHttpRequest();
-    http.onload = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(this.responseText)
-            if (id) {
-                titleInput.value = response.title
-                imageInput.value = response.image
-                contentInput.value = response.content
-            }
-            else {
-                displayData(response)
-            }
-        }
-    }
-    http.open("GET", id ? `${url}/${id}` : url)
-    http.send()
+async function getData() {
+    await apiCall("GET", url).then((data) => {
+        displayData(data);
+    }).catch(function (err) {
+        console.log(err);
+    })
 }
 
 document.querySelector('.form-container').addEventListener('submit', submitControl)
@@ -59,69 +50,41 @@ async function submitControl(event) {
         content: this.elements['content'].value
     });
     if (id) {
-        await editAtApi(id, obj)
+        await apiCall("PUT", `${url}/${id}`, obj).then((data) => {
+            getData()
+            popup.style.display = 'none'
+        }).catch(function (err) {
+            console.log(err);
+        })
         id = null
     }
     else {
-        await postData(obj)
+        await apiCall("POST", url, obj).then((data) => {
+            getData()
+        }).catch(function (err) {
+            console.log(err);
+        })
     }
     this.reset()
 }
-//POST method
-async function postData(obj) {
-    const http = new XMLHttpRequest();
-    http.open('POST', url);
-    http.responseType = 'json'
-    http.setRequestHeader('Content-Type', 'application/json');
-    http.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status === 201) {
-                getData()
-            }
-        }
-    }
-    http.send(obj);
-}
-//PUT method
-async function editAtApi(id, obj) {
-    const http = new XMLHttpRequest();
-    http.open('PUT', `${url}/${id}`);
-    http.responseType = 'json'
-    http.setRequestHeader('Content-Type', 'application/json');
-    http.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status === 200) {
-                popup.style.display = 'none'
-                getData()
-            }
-        }
-    }
-    http.send(obj);
-}
-//DELETE method
-async function deleteAtApi(id) {
-    const http = new XMLHttpRequest();
-    http.open('DELETE', `${url}/${id}`);
-    http.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status === 200) {
-                getData()
-                id = null
-            }
-        }
-    }
-    http.send();
-}
+
 //Control Delete and Edit button
 async function controlItem(event) {
     if (event.target.classList.contains('remove-item')) {
         id = event.target.dataset.id
-        await deleteAtApi(id)
+        await apiCall("DELETE", `${url}/${id}`).then((data) => {
+            getData()
+        })
+        id = null
     }
     else if (event.target.classList.contains('edit-item')) {
         id = event.target.dataset.id
         popup.style.display = 'block'
-        getData(id)
+        await apiCall("GET", `${url}/${id}`).then((data) => {
+            titleInput.value = data.title
+            imageInput.value = data.image
+            contentInput.value = data.content
+        })
     }
 }
 list.addEventListener('click', controlItem)
