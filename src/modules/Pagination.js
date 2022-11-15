ae5d576ccb8722b0a66ab5b52e89e28673d5a53c
Thien Phu <nttp.220401@gmail.com>
2022-11-15 08:36:02 +0700

    15/11

--- src/modules/Pagination.js
+++
@@ -1,9 +0,0 @@
-export default function displayList(items, wrapper, rows_per_page, page) {
-    wrapper.innerHTML = "";
-    page--;
-    let loop_start = rows_per_page * page
-    let paginatedItems = items.slice(loop_start + rows_per_page)
-    console.log(paginatedItems);
-    for (let i = loop_start; i < loop_start; i++) {
-    }
-}
